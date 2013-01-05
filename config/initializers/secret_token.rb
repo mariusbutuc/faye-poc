396f8f68a216c7664220fcfda6532e77e2d30173
Marius Butuc <marius.butuc@gmail.com>
2013-01-05 13:36:14 -0500

    Vanilla Rails 3.2.10 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayePoc::Application.config.secret_token = 'd393b223ae38fe94daa0292e3a504d4b5789b45bc28d51952fe6bf10e5ee5e1ab0dfe28124a73ec755061b0a72d69896c6c81c955d7c172d631c082cb9de40bc'
